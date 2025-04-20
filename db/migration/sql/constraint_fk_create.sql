31514acba17d5a22627097a9266e14af7c0f3eae
Daniel Langio <langio.tehiniavo@gmail.com>
2025-04-20 11:45:46 +0300

    feat(spring-boot): initialize commit

--- db/migration/sql/constraint_fk_create.sql
+++ db/migration/sql/constraint_fk_create.sql
@@ -1,14 +1,16 @@
 -- object: fk_activity_category_id | type: CONSTRAINT --
 -- ALTER TABLE public.activity DROP CONSTRAINT IF EXISTS fk_activity_category_id CASCADE;
-ALTER TABLE public.activity ADD CONSTRAINT fk_activity_category_id FOREIGN KEY (category_id)
-REFERENCES public.activity_category (id) MATCH SIMPLE
-ON DELETE SET NULL ON UPDATE NO ACTION;
+ALTER TABLE public.activity
+    ADD CONSTRAINT fk_activity_category_id FOREIGN KEY (category_id)
+        REFERENCES public.activity_category (id) MATCH SIMPLE
+        ON DELETE SET NULL ON UPDATE NO ACTION;
 -- ddl-end --
 
 -- object: fk_activity_account_id | type: CONSTRAINT --
 -- ALTER TABLE public.activity DROP CONSTRAINT IF EXISTS fk_activity_account_id CASCADE;
-ALTER TABLE public.activity ADD CONSTRAINT fk_activity_account_id FOREIGN KEY (account_id)
-REFERENCES public.account (id) MATCH SIMPLE
-ON DELETE SET NULL ON UPDATE NO ACTION;
+ALTER TABLE public.activity
+    ADD CONSTRAINT fk_activity_account_id FOREIGN KEY (account_id)
+        REFERENCES public.account (id) MATCH SIMPLE
+        ON DELETE SET NULL ON UPDATE NO ACTION;
 -- ddl-end --
 
